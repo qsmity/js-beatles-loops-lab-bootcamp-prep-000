8ddb68e3e87b2e0c4281dcf911f98c4c7658f895
alpha-acrobat-9971 <qsmity@gmail.com>
2020-03-31 08:03:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // add solution here
+
+var music = []
+var musicians = ["justin", "carl", "chester"]
+var instruments = ["guitar", "flute", "piano"]
+
+var theBeatlesPlay = function(musicians, instruments){
+  for (let i = 0; i < musicians.length; i++){
+    const array =   music.push(musicians[i] + " plays " + instruments[i])
+  }
+    return array
+}
+
+
+console.log(theBeatlesPlay(musicians, instruments))
